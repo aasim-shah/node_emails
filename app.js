c6937192a414633e8c3ccc62ee3d390ad4cd3960
Glitch (lively-bony-twine) <none>
2021-12-04 20:41:16 +0000

    🎟👱 Checkpoint ./app.js:39277590/260 ./package.json:39277590/695

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 const express = require('express')
+const util = require('util')
 const app = express()
 const mysql = require('mysql2');
 const port =  process.env.PORT || 8080;
 
 const pool = mysql.createPool({
-    host: '',
+    host: '65.21.118.123',
     user: 'specscam_prouser',
     password: 'Mardan8110',
     database: 'specscam_phones'
@@ -22,6 +23,9 @@ app.get('/' ,async (req , res)=> {
         pool.query('SELECT * FROM news' , (err , rows , fields)=>{
           console.log(err);
           res.json(err);
+          if (err.code === 'ETIMEDOUT') {
+    console.log('My dish error: ', util.inspect(err, { showHidden: true, depth: 2 }));
+}
           
           
         })
