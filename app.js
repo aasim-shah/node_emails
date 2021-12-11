75145f28e38aba639b5602049c12033e6bca9056
Glitch (lively-bony-twine) <none>
2021-12-11 18:57:41 +0000

    💎🚿 Checkpoint ./app.js:39277590/2391

--- app.js
+++ app.js
@@ -3,29 +3,32 @@ const app = express()
 var nodemailer = require('nodemailer');
 const port =  process.env.PORT || 8080;
 var transporter = nodemailer.createTransport({
-  service: 'gmail',
+   host: 'smtp.gmail.com',
+     port: 587,
+     secure: false,
   auth: {
-    user: 'syedaasimshah1@gmail.com',
-    pass: 'yourpassword'
+    user: 'asimshah8110@gmail.com',
+    pass: 'lewani007'
   }
 });
 
 var mailOptions = {
-  from: 'youremail@gmail.com',
+  from: 'syedaasimshah1@@gmail.com',
   to: 'myfriend@yahoo.com',
   subject: 'Sending Email using Node.js',
   text: 'That was easy!'
 };
 
-transporter.sendMail(mailOptions, function(error, info){
+
+app.get('/' ,async (req , res)=> {
+ transporter.sendMail(mailOptions, function(error, info){
   if (error) {
-    console.log(error);
+    res.send(error);
+  console.log('last errror')
   } else {
     console.log('Email sent: ' + info.response);
   }
 });
-app.get('/' ,async (req , res)=> {
-  res.send('okay')
 })
 
 
